77fc67ec0e9d85399fa722bd7a65bfa1ed59ca7b
jaw <62795911+JacobWestby@users.noreply.github.com>
2022-05-23 23:07:15 +0200

    initial commit part2, redoing with react

---
+++ portfolio/src/App.js
@@ -0,0 +1,14 @@
+import { Router, Routes, BrowserRouter } from 'react-router-dom';
+
+import './styles/reset.css';
+import './styles/index.css';
+
+function App() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default App;
