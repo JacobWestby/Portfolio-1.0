50128107e329494f07e46c028174f7321ec3d2bf
jaw <62795911+JacobWestby@users.noreply.github.com>
2022-05-27 01:57:17 +0200

    added projects grid

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -1,21 +1,29 @@
 import Nav from "../reusableComponents/Nav"
 import CenteredCont from "../reusableComponents/CenteredCont"
 import jacobWestby from "../images/jacob-westby.jpg";
+import Projects from "./Projects";
 
 const Home = () => {
     return (
-        <header>
-            <Nav />
-            <CenteredCont>
-                <div className="home__content">
-                    <img src={jacobWestby} alt="Jacob Westby" />
-                    <div className="home__content-text">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti quo dolorem adipisci deserunt ullam vero eveniet sed possimus reprehenderit quidem provident, aut modi amet sunt quos perferendis consequatur asperiores! Reprehenderit?</p>
+        <>
+            <header>
+                <Nav />
+                <CenteredCont>
+                    <div className="home__content">
+                        <img src={jacobWestby} alt="Jacob Westby" />
+                        <div className="home__content-text">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti quo dolorem adipisci deserunt ullam vero eveniet sed possimus reprehenderit quidem provident, aut modi amet sunt quos perferendis consequatur asperiores! Reprehenderit?</p>
+                        </div>
                     </div>
-                </div>
+                </CenteredCont>
+            </header>
+            <CenteredCont>
+                <main>
+                    <Projects />
+                </main>
             </CenteredCont>
-        </header>
+        </>
     )
 }
 
-export default Home
+export default Home;
---
+++ portfolio/src/components/Projects.js
@@ -0,0 +1,15 @@
+
+const Projects = () => {
+    return (
+        <div className="projects">
+            <h3>Projects</h3>
+            <div className="projects__grid">
+                <div className="gridItem1">Hello</div>
+                <div className="gridItem2">Hello</div>
+                <div className="gridItem3">Hello</div>
+            </div>
+        </div>
+    )
+}
+
+export default Projects
