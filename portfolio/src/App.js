e7b85cbecc5da6bc67bc5052a901a94b837da3d1
jaw <62795911+JacobWestby@users.noreply.github.com>
2022-07-20 15:46:28 +0200

    added bhramon project and styling

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,8 +1,13 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
+// COMPONENTS
 import Home from './components/Home.js';
 import GenesisProject from './components/GenesisProject.js';
+import MucisianProject from './components/MucisianProject.js';
+import ProductlyProject from './components/ProductlyProject.js';
+import BhramonProject from './components/BhramonProject.js';
 
+// IMAGES
 import reactGame from './images/reactGame.png';
 import genesis from './images/genesis_cropped.jpg';
 import backAccount from './images/bankAccount.png';
@@ -12,70 +17,51 @@ import mucisian from './images/mucisian.png';
 import productly from './images/productly_cropped.png';
 import AboutPage from './components/AboutPage.js';
 import ContactPage from './components/ContactPage.js';
-import rawHTML from './projects/HTMLList.js';
-import MucisianProject from './components/MucisianProject.js';
-import ProductlyProject from './components/ProductlyProject.js';
+
 
 
 function App() {
 
-  // Set state display html in app. use id from handleclick to set array index
-  // pass display html state to displayHtml component.
-
-  // const [instaPics, setInstaPics] = useState([{}])
-
+  // PROJECTS ARRAY 
   const projects = [
     {
       img: mucisian,
       id: 1,
       name: "mucisian",
-      html: rawHTML[1].html
     },
     {
       img: genesis,
       id: 2,
       name: "genesis",
-      html: rawHTML[2].html
-
     },
     {
       img: reactGame,
       id: 3,
       name: "reactGame",
-      html: rawHTML[3].html
-
     },
     {
       img: productly,
       id: 4,
       name: "productly",
-      html: rawHTML[4].html
-
-
     },
     {
       img: feedback,
       id: 5,
       name: "feedback",
-      html: rawHTML[5].html
-
     },
     {
       img: bhramon,
       id: 6,
       name: "bhramon",
-      html: rawHTML[6].html
-
     },
     {
       img: backAccount,
       id: 7,
       name: "backAccount",
-      html: rawHTML[7].html
-
     }
   ];
 
+  // CALCULATE UP-TO-DATE AGE
   const calcAge = () => {
     const date = new Date()
     const currentAge = date.getFullYear() - 1993;
@@ -83,28 +69,16 @@ function App() {
     return currentAge;
   };
 
-  // useEffect(() => {
-  //   getGram();
-  // }, []);
-
-  // const getGram = async () => {
-  //   const response = await fetch('https://graph.instagram.com/v11.0/andreahansson_/media');
-  //   const data = await response.json()
-
-  //   setInstaPics(data);
-  // };
-
-  // console.log(instaPics)
-
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Home calcAge={calcAge()} projects={projects} />} />
         <Route path="/components/aboutpage" element={<AboutPage />} />
         <Route path="/components/contactpage" element={<ContactPage />} />
-        <Route path="/components/mucisianProject" element={<MucisianProject />} />
+        <Route path="/components/mucisianproject" element={<MucisianProject />} />
         <Route path="/components/genesisproject" element={<GenesisProject />} />
         <Route path="/components/productlyproject" element={<ProductlyProject />} />
+        <Route path="/components/bhramonproject" element={<BhramonProject />} />
       </Routes>
     </BrowserRouter>
   );
