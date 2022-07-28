c34606214cab8959584a309aecd62348c743c388
jaw <62795911+JacobWestby@users.noreply.github.com>
2022-07-28 15:56:50 +0200

    added styling and personal text for about page

--- portfolio/src/components/AboutPage.js
+++ portfolio/src/components/AboutPage.js
@@ -2,44 +2,59 @@ import CenteredCont from "./reusableComponents/CenteredCont"
 import Footer from "./reusableComponents/Footer";
 
 import jacobWestby2 from '../images/jacobWestby2.jpg';
+import menuBurger from '../images/menuBurger.svg';
 import ourVan2 from '../images/ourVan2.jpg';
 import Nav from "./reusableComponents/Nav";
 import icons8Github from "../images/icons8Github.svg";
 import icons8Linkedin from "../images/icons8Linkedin.svg";
+import about_me_header from "../images/about_me_header.svg";
 
 const AboutPage = () => {
     return (
         <>
-            <Nav />
+            <Nav menuBurger={menuBurger} />
             <div className="about-background">
                 <CenteredCont>
                     <header>
                         <div className="about__cont-top">
-                            <img src={jacobWestby2} alt="Jacob Westby eating icecream" />
+                            <h2>About Me</h2>
+                            <img src={about_me_header} alt="Jacob Westby eating icecream" />
                             <div className="about__cont-top-text">
-                                <h2>Jacob Westby</h2>
-                                <p>- Get to know me</p>
+                                <p>Jacob Westby</p>
                             </div>
                         </div>
                     </header>
                     <main className="about__main">
+                        <h3 className="about__main-heading-top">What i did before</h3>
                         <div className="about__main__cont-1">
                             <div className="about__main__cont-1-text">
-                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates soluta, quam non hic corporis laborum nulla repellendus aut quo impedit nemo molestiae totam autem optio ea iure! Similique, illum voluptate!</p>
-                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic nisi optio eveniet laboriosam explicabo illo odio nobis consequatur dolorem esse, iste tempora iusto obcaecati molestiae quibusdam dignissimos! Quam, minus recusandae.</p>
+                                <p>Before I started my web development journey I was working as a Lighttechnician in the event industry. Being a lighttech is a very demanding job both physically and mentally, there are a lot of heavy things to carry and push around and there are a lot of problems to solve.
+                                </p>
+                                <p>I worked in the event industry for several years but my last position before corona hit was at Stockholm International Fairs as Head of Lights and the fairs only Lightoperator.
+                                </p>
                             </div>
-                            <div className="about__main__cont-1__img-cont">
+                            {/* <div className="about__main__cont-1__img-cont">
                                 <img src={ourVan2} alt="Mercedes sprinter van" className="about__main__cont-1__img-cont_img1" />
                                 <img src={jacobWestby2} alt="" className="about__main__cont-1__img-cont_img2" />
-                            </div>
+                            </div> */}
                         </div>
-
+                        <h3 className="about__main-heading-mid">What i've studied</h3>
                         <div className="about__main__cont-2">
                             <div className="about__main__cont-2-text">
-                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates soluta, quam non hic corporis laborum nulla repellendus aut quo impedit nemo molestiae totam autem optio ea iure! Similique, illum voluptate!</p>
-                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic nisi optio eveniet laboriosam explicabo illo odio nobis consequatur dolorem esse, iste tempora iusto obcaecati molestiae quibusdam dignissimos! Quam, minus recusandae.</p>
+                                <p>When corona hit, my last job shut down rather quickly and showed no signs of starting back up. So I started taking online courses in web development. I started with freecodecamp and some apps but soon found my way to Codecademy where I paid for a pro membership and really got into it.</p>
+                                <p>I completed 85% of codecademy's fullstack course before some life stuff got in the way that led me to taking a break from coding. At this point I had been coding everyday for almost 1 year but working and building our van left no time for me to continue studying.</p>
+                                <p>A few months after moving into the van I started an intensive front-end course that went on for 6 months which ended in May 2022.</p>
                             </div>
-                            <img src={ourVan2} alt="Mercedes sprinter van" className="van" />
+                            {/* <img src={ourVan2} alt="Mercedes sprinter van" className="van" /> */}
+                        </div>
+                        <h3 className="about__main-heading-bottom">What i do now</h3>
+                        <div className="about__main__cont-3">
+                            <div className="about__main__cont-3-text">
+                                <p>Right now me and my fiance are still living in the van with our dogs and traveling around. I take temporary jobs when possible to have an income but the end goal is to earn a good income from doing web development work remotely.<br />
+                                    To that end I am doing some kind of coding everyday, whether that is working on a new or old project, improving my portfolio or contacting possible costommers.
+                                </p>
+                            </div>
+                            {/* <img src={ourVan2} alt="Mercedes sprinter van" className="van" /> */}
                         </div>
                     </main>
                 </CenteredCont>
--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -1,52 +1,19 @@
-import { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-
 const FORM_ENDPOINT = "https://public.herotofu.com/v1/ce433710-0918-11ed-be50-e78da9ee852d"
 
-
 const Contact = () => {
-    const [initialState, setInitialState] = useState({
-        name: "",
-        email: "",
-        message: "",
-        id: ""
-    });
-
-    const handleChange = (e) => {
-        const name = e.target.name;
-        const value = e.target.value;
-
-        setInitialState({ ...initialState, [name]: value })
-    };
-
-    const [message, setMessage] = useState({});
-
-    const sendMessage = (e) => {
-        e.preventDefault();
-
-        setMessage({
-            name: initialState.name,
-            email: initialState.email,
-            message: initialState.message,
-            id: uuidv4(),
-        })
-
-        console.log(message);
-    };
-
     return (
         <div className="contact">
             <div className="contact__form">
                 <h4>contact me</h4>
                 <form action={FORM_ENDPOINT} method="POST">
                     <label htmlFor="name">Name</label>
-                    <input value={initialState.name} name="name" type="text" required placeholder="Your name.." onChange={handleChange} />
+                    <input name="name" type="text" required placeholder="Your name.." />
 
                     <label htmlFor="email">Email</label>
-                    <input value={initialState.email} name="email" type="text" required placeholder="Your email.." onChange={handleChange} />
+                    <input name="email" type="text" required placeholder="Your email.." />
 
                     <label htmlFor="message">Message</label>
-                    <input value={initialState.message} name="message" type="textarea" required placeholder="Your question or message here.." onChange={handleChange} />
+                    <input name="message" type="textarea" required placeholder="Your question or message here.." />
                     <button type="submit">Send</button>
                 </form>
             </div>
